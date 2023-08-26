f623bf337f7c2bfcccfed4d1bf6caa1a89c6b7c8
Кирилл Мастюгин <k.mastyugin@itlc.ru>
2023-08-26 23:56:57 +0300

    Адрес сайта вынесен в переменную окружения

--- src/modules/auth/steam/index.ts
+++ src/modules/auth/steam/index.ts
@@ -4,8 +4,8 @@ import SteamAuth from "node-steam-openid";
 const API_KEY = process.env.STEAM_APIKEY;
 
 const steam = new SteamAuth({
-  realm: "http://localhost:3000",
-  returnUrl: "http://localhost:3000/api/auth/steam/authenticate",
+  realm: process.env.DOMEN,
+  returnUrl: process.env.DOMEN + "/api/auth/steam/authenticate",
   apiKey: API_KEY,
 });
 
